49c8cc44d35979d91578404a1abdd19a5eba03ca
kcharris <kcharris77@gmail.com>
2019-03-16 08:07:18 -0500

    add square class and edit Board.new_game to comply

--- lib/ruby_chess.rb
+++ lib/ruby_chess.rb
@@ -3,6 +3,14 @@ module RubyChess
     attr_reader :output
     attr_accessor :grid
 
+    class Square
+      attr_accessor :value
+
+      def initialize(value = nil)
+        @value = value
+      end
+    end
+
     def initialize
       @grid = {}
       @output = nil
@@ -11,40 +19,39 @@ module RubyChess
 
     def new_game
       #empty places are marked with check here.
+
       8.times do |x|
         8.times do |y|
           case
           when y == 2
-            @grid[x.to_s + y.to_s] = Piece.new("p", "w")
+            @grid[x.to_s + y.to_s].value = Piece.new("p", "w")
           when y == 7
-            @grid[x.to_s + y.to_s] = Piece.new("p", "b")
-          when y > 2 && y < 7
-            @grid[x.to_s + y.to_s] = "check"
+            @grid[x.to_s + y.to_s].value = Piece.new("p", "b")
           when y == 1
             case
             when x == 1 || x == 8
-              @grid[x.to_s + y.to_s] = Piece.new("r", "w")
+              @grid[x.to_s + y.to_s].value = Piece.new("r", "w")
             when x == 2 || x == 7
-              @grid[x.to_s + y.to_s] = Piece.new("k", "w")
+              @grid[x.to_s + y.to_s].value = Piece.new("k", "w")
             when x == 3 || x == 6
-              @grid[x.to_s + y.to_s] = Piece.new("b", "w")
+              @grid[x.to_s + y.to_s].value = Piece.new("b", "w")
             when x == 4
-              @grid[x.to_s + y.to_s] = Piece.new("Q", "w")
+              @grid[x.to_s + y.to_s].value = Piece.new("Q", "w")
             when x == 5
-              @grid[x.to_s + y.to_s] = Piece.new("K", "w")
+              @grid[x.to_s + y.to_s].value = Piece.new("K", "w")
             end
           when y == 7
             case
             when x == 1 || x == 8
-              @grid[x.to_s + y.to_s] = Piece.new("r", "b")
+              @grid[x.to_s + y.to_s].value = Piece.new("r", "b")
             when x == 2 || x == 7
-              @grid[x.to_s + y.to_s] = Piece.new("k", "b")
+              @grid[x.to_s + y.to_s].value = Piece.new("k", "b")
             when x == 3 || x == 6
-              @grid[x.to_s + y.to_s] = Piece.new("b", "b")
+              @grid[x.to_s + y.to_s].value = Piece.new("b", "b")
             when x == 4
-              @grid[x.to_s + y.to_s] = Piece.new("Q", "b")
+              @grid[x.to_s + y.to_s].value = Piece.new("Q", "b")
             when x == 5
-              @grid[x.to_s + y.to_s] = Piece.new("K", "b")
+              @grid[x.to_s + y.to_s].value = Piece.new("K", "b")
             end
           end
         end
