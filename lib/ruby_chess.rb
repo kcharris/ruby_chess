78e136efe92d25de6c9864d17584117ec42e1da9
kcharris <kcharris77@gmail.com>
2019-03-17 12:28:02 -0500

    swith alphabet pieces with utf symbols and fix new_game to create row 8

--- lib/ruby_chess.rb
+++ lib/ruby_chess.rb
@@ -15,36 +15,36 @@ module RubyChess
         8.times do |y|
           case
           when y == 2
-            @grid[x.to_s + y.to_s] = Piece.new("p", "w")
+            @grid[x.to_s + y.to_s] = Piece.new("u\2659")
           when y == 7
-            @grid[x.to_s + y.to_s] = Piece.new("p", "b")
+            @grid[x.to_s + y.to_s] = Piece.new("u\265F")
           when y > 2 && y < 7
-            @grid[x.to_s + y.to_s] = Piece.new("_", "_")
+            @grid[x.to_s + y.to_s] = Piece.new("_")
           when y == 1
             case
             when x == 1 || x == 8
-              @grid[x.to_s + y.to_s] = Piece.new("r", "w")
+              @grid[x.to_s + y.to_s] = Piece.new("u\2656")
             when x == 2 || x == 7
-              @grid[x.to_s + y.to_s] = Piece.new("k", "w")
+              @grid[x.to_s + y.to_s] = Piece.new("u\2658")
             when x == 3 || x == 6
-              @grid[x.to_s + y.to_s] = Piece.new("b", "w")
+              @grid[x.to_s + y.to_s] = Piece.new("u\2657")
             when x == 4
-              @grid[x.to_s + y.to_s] = Piece.new("Q", "w")
+              @grid[x.to_s + y.to_s] = Piece.new("u\2655")
             when x == 5
-              @grid[x.to_s + y.to_s] = Piece.new("K", "w")
+              @grid[x.to_s + y.to_s] = Piece.new("u\2654")
             end
-          when y == 7
+          when y == 8
             case
             when x == 1 || x == 8
-              @grid[x.to_s + y.to_s] = Piece.new("r", "b")
+              @grid[x.to_s + y.to_s] = Piece.new("u\265C")
             when x == 2 || x == 7
-              @grid[x.to_s + y.to_s] = Piece.new("k", "b")
+              @grid[x.to_s + y.to_s] = Piece.new("u\265E")
             when x == 3 || x == 6
-              @grid[x.to_s + y.to_s] = Piece.new("b", "b")
+              @grid[x.to_s + y.to_s] = Piece.new("u\265D")
             when x == 4
-              @grid[x.to_s + y.to_s] = Piece.new("Q", "b")
+              @grid[x.to_s + y.to_s] = Piece.new("u\265B")
             when x == 5
-              @grid[x.to_s + y.to_s] = Piece.new("K", "b")
+              @grid[x.to_s + y.to_s] = Piece.new("u\265A")
             end
           end
         end
@@ -53,12 +53,14 @@ module RubyChess
   end
 
   class Piece
-    attr_reader :name
+    attr_reader :read
 
-    def initialize(type, color)
-      @type = type
-      @color = color
-      @name = color.upcase + @type
+    def initialize(piece)
+      @read = piece.encode("utf-8")
     end
   end
 end
+
+w_king = "u\2659"
+
+puts w_king.encode("utf-8")
